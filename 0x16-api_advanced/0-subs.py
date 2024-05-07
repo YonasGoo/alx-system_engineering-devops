c131a8ac9cd774209eef402c418e6993f7ddedc3
pwntheweb <ezra005@proton.me>
2024-05-07 22:25:24 +0000

    task-0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,36 +1,16 @@
 #!/usr/bin/python3
-
+""" function that queries the Reddit API and returns the number
+of subscribers (not active users, total subscribers) for a given subreddit.
 """
-Query a subreddit and return the number of
-total subscribers in that subredit
-"""
-
 from requests import get
-from sys import argv
 
 
-headers = {
-    "User-Agent": "Of course I had to use a custom User-Agent",
-    "X-Forwared-For": "iamthecavalry"
-}
-
-
-def number_of_subscribers(subreddit: str) -> int:
-    """
-    Query the subreddit and return the number of
-    Active subs. If its an invalid subredit, return 0
-    """
-    response = get("https://www.reddit.com/r/{}/about.json".format(subreddit),
-                   headers=headers)
-    data = response.json()
-    try:
-        if 'error' in data.keys():
-            return 0
-        else:
-            return data['data']['subscribers']
-    except Exception as e:
-        return 0
-
-
-if __name__ == "__main__":
-    print(number_of_subscribers(argv[1]))
+def number_of_subscribers(subreddit):
+    """ returns the number of subscribers"""
+    url = 'https://www.reddit.com'
+    path = '/r/{}/about.json'.format(subreddit)
+    header = {'user-agent': 'jormao'}
+    r = get(url + path, headers=header).json()
+    if r.get('data') and r.get('data').get('subscribers'):
+        return (r['data']['subscribers'])
+    return (0)
