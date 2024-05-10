0cfa27f450e8cf67478d9b038bdfe797a5dd316c
pwntheweb <ezra005@proton.me>
2024-05-10 02:55:45 +0000

    deleted

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++
@@ -1,6 +0,0 @@
-# automate the task of creating a custom 
-# HTTP header with puppet
-exec {'configure HTTP':
-  command => "apt-get update && apt-get -y install nginx && echo -e 'Holberton School' > /var/www/html/index.html && sed -i '43i rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;' /etc/nginx/sites-available/default && sed -i '12i add_header X-Served-By $hostname;' /etc/nginx/nginx.conf && service nginx start",
-  provider => 'shell',
-}
