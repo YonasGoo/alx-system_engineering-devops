9842c0a6054f1030d9a1afc35f56f0ba015d3743
Zyonasec <ezra005@proton.me>
2024-06-29 00:18:44 +0300

    fixes

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,12 +1,3 @@
 #kills killmeow
 exec {'pkill killmenow':
-	command		=> '/usr/bin/pkill killmenow',
-	path		=> ['/bin', '/usr/bin'],
-	refreshonly	=> true,
-	subscribe	=> Notify['kill process killmenow'],
 }
-
-notify {'kill process killmenow':
-	message		=> 'killing process killmenow'
-}
-``
